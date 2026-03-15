16c1e08b4fa1abc90457fe0731e104033183b425
kamogelo matsomela <kamogelo.g.matsomela.05@gmial.com>
2026-03-15 23:14:06 +0200

    Add ConfirmDialog

---
+++ VtmTool.UI/ConfirmDialog.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace VtmTool.UI;
+
+public partial class ConfirmDialog : Window
+{
+    public bool Confirmed { get; private set; }
+
+    public ConfirmDialog(string message)
+    {
+        InitializeComponent();
+        MessageText.Text = message;
+    }
+
+    void OnConfirm(object? sender, RoutedEventArgs e) { Confirmed = true; Close(); }
+    void OnCancel(object? sender, RoutedEventArgs e) => Close();
+}
